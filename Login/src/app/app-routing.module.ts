2baffbd86633377c1b1cc77013c77870358156ed
agustinezequielgomez <agusgomez99@gmail.com>
2019-08-30 19:28:55 -0300

    Login en progreso

--- Login/src/app/app-routing.module.ts
+++ Login/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent},
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
