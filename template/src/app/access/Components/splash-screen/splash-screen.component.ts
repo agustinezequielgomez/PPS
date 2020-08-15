5d723527d65892b2cb4810aceac0537782d05be9
agustinezequielgomez <agusgomez99@gmail.com>
2020-08-15 15:58:47 -0300

    Forgot to upload file

--- template/src/app/access/Components/splash-screen/splash-screen.component.ts
+++ template/src/app/access/Components/splash-screen/splash-screen.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { AnimationService } from '../../../core/Services/animation.service';
+import { Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
   styleUrls: ['./splash-screen.component.scss'],
 })
-export class SplashScreenComponent implements OnInit {
+export class SplashScreenComponent implements OnInit, AfterViewInit {
 
+  @Output() destroyComponent = new EventEmitter<void>();
   constructor(private router: Router) {
     setTimeout(() => this.router.navigate(['login']), 5000);
    }
+
+   async ngAfterViewInit() {
+    await AnimationService.playChainedAnimations();
+    this.destroyComponent.emit();
+  }
+
 
   ngOnInit() {}
 
