a71f0cf2ae477e114d349f0f3788b66854495e51
agustinezequielgomez <agusgomez99@gmail.com>
2020-08-21 21:15:06 -0300

    Updated android config service: Replaced 3rd party extensions with Capacitor native ones

--- template/src/app/core/Services/config/android-config.service.ts
+++ template/src/app/core/Services/config/android-config.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AndroidPermissions as PermissionService } from '@ionic-native/android-permissions/ngx';
 import { NavigationBar } from '@ionic-native/navigation-bar/ngx';
-import { StatusBar as StatusBarService } from '@ionic-native/status-bar/ngx';
-import { AppConfig, StatusBar } from '../../Models/Classes/app-config';
+import { AppConfig, StatusBar as StatusBarModel } from '../../Models/Classes/app-config';
 import { StorageKeys } from '../../Models/Enums/storage-keys.enum';
 import { DataShareService } from '../data-share.service';
 import { IConfigService } from '../Interfaces/iconfig-service';
 import { StorageService } from '../storage.service';
+import { Plugins, StatusBarAnimation } from '@capacitor/core';
+const { StatusBar } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AndroidConfigService implements IConfigService {
 
-  constructor(private storage: StorageService, private statusBarService: StatusBarService, private navBar: NavigationBar,
+  constructor(private storage: StorageService, private navBar: NavigationBar,
               private permissions: PermissionService, private dataShare: DataShareService) { }
 
   async loadAppConfiguration(): Promise<void> {
     try {
       const config = await this.storage.getStorage<AppConfig>(StorageKeys.CONFIG);
-      this.customizeStatusBar(config.statusBar);
+      await this.customizeStatusBar(config.statusBar);
       await this.getPermisions(config.permissions);
       this.dataShare.SetFullScreen = (config.navigationBar && config.statusBar.enabled);
       console.log('reached the end withour erroring!');
@@ -31,11 +32,11 @@ export class AndroidConfigService implements IConfigService {
   async toggleFullScreenMode() {
     try {
       if (this.dataShare.IsFullScreen) {
-        await this.statusBarService.hide();
+        await StatusBar.hide({animation: StatusBarAnimation.Fade});
         await this.navBar.setUp(true);
         this.dataShare.SetFullScreen = true;
       } else {
-        await this.statusBarService.show();
+        await StatusBar.show({animation: StatusBarAnimation.Fade});
         await this.navBar.setUp(false);
         this.dataShare.SetFullScreen = false;
       }
@@ -60,11 +61,11 @@ export class AndroidConfigService implements IConfigService {
     this.navBar.setUp(enable);
   }
 
-  customizeStatusBar(statusBar: StatusBar) {
+  async customizeStatusBar(statusBar: StatusBarModel) {
     if (statusBar.enabled) {
-      this.statusBarService.backgroundColorByHexString(statusBar.color);
+      StatusBar.setBackgroundColor({color: statusBar.color});
     } else {
-      this.statusBarService.hide();
+      await StatusBar.hide({animation: StatusBarAnimation.Fade});
     }
   }
 
